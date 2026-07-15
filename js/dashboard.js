7745097aafdb93884c94fa4adc5445b12ad55c95
Salman <lezzygithub@gmail.com>
2026-07-15 08:30:33 +0700

    feat: add live time feature

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,6 +4,8 @@ import { darkModeButton } from "./utils/darkmode.js";
 
 const logoutBtn = getId("logoutBtn");
 const userEmail = getId("userEmail");
+const liveDate = getId("liveDate");
+const liveTime = getId("liveTime");
 
 darkModeButton();
 
@@ -11,6 +13,19 @@ function logoutUser() {
   removeStorage("isLogin");
   removeStorage("currentUser");
 }
+
+function updateLiveTime() {
+  const now = new Date();
+  liveDate.textContent = now.toLocaleDateString("id-ID", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  liveTime.textContent = now.toLocaleTimeString("id-ID");
+}
+updateLiveTime()
+setInterval(updateLiveTime, 1000);
 
 if (window.location.pathname.includes("dashboard.html")) {
   const isLogin = getStorage("isLogin");
